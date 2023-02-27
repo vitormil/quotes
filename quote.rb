d0c607c7f63566eb3f27e759fd68824b9023e8dd
Vitor Oliveira <vitor.oliveira@trustly.com>
2023-02-27 12:01:10 +0000

    Add --color

--- quote.rb
+++ quote.rb
@@ -1,6 +1,11 @@
 #!/usr/bin/ruby
 require 'json'
 
+color = false
+ARGV.each do|a|
+  color = true if a == "--color"
+end
+
 # https://raw.githubusercontent.com/mubaris/motivate/master/motivate/data_unique/unique_quotes.json
 file = File.read("#{__dir__}/data.json")
 data_hash = JSON.parse(file)
@@ -11,4 +16,8 @@ begin
   author = data["author"]
 end while quote.length > 100
 
-puts "#{author}: #{quote}"
+if color
+  puts "\033[1;33m#{author}\033[1;30m:\033[0m \033[34m#{quote} \033[0m"
+else
+  puts "#{author}: #{quote}"
+end
